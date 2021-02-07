f572a11276b3be9253347aee32e36b414b713ed9
julietvt <freshitpt@gmail.com>
2021-02-07 11:42:47 +0200

    some example async

---
+++ assets/index.js
@@ -0,0 +1,19 @@
+const square = document.getElementById('item');
+let deg = 0;
+
+/*
+setInterval(function () {
+  square.style.transform = `rotate(${++deg}deg)`;
+}, 1000 / 60);
+*/
+
+setInterval(rotateFun, 1000 / 60);
+
+function rotateFun() {
+  square.style.transform = `rotate(${++deg}deg)`;
+}
+
+const button = document.getElementsByTagName('button')[0];
+button.onclick = function () {
+  alert('hello');
+};
