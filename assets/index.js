8d531b312175eea587346a9c26c1c375e53bf477
julietvt <freshitpt@gmail.com>
2021-02-07 12:26:26 +0200

    XMLHttpRequest

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,6 @@
+/*
 const square = document.getElementById('item');
 let deg = 0;
-
-/*
-setInterval(function () {
-  square.style.transform = `rotate(${++deg}deg)`;
-}, 1000 / 60);
-*/
 
 setInterval(rotateFun, 1000 / 60);
 
@@ -17,3 +12,38 @@ const button = document.getElementsByTagName('button')[0];
 button.onclick = function () {
   alert('hello');
 };
+*/
+
+const store = {
+  users: null,
+  isFetching: false,
+  error: null,
+};
+
+// асинхронный код выполняется после синхронного!!!
+
+//https://developer.mozilla.org/ru/docs/Web/API/XMLHttpRequest
+const request = new XMLHttpRequest();
+
+const button = document.getElementById('loadButton');
+button.onclick = function () {
+  // false - синхрон загрузка, true (-) - асинхр
+  request.open('GET', './data/users.json', true);
+  request.send();
+};
+
+request.onloadstart = function () {
+  store.isFetching = true;
+  //console.log(store);
+};
+
+request.onload = function () {
+  store.isFetching = false;
+  if (this.status >= 200 && this.status < 300) {
+    store.users = JSON.parse(this.responseText);
+  } else {
+    store.error = new Error(`${this.status} : ${this.statusText}`);
+  }
+};
+
+console.log(store);
